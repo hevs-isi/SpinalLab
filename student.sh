4b22349b715bd1250fcaeb04139a89ad15a1e64f
miaou <miaou>
2019-04-14 12:06:54 +0200

    more cleaning

--- student.sh
+++ student.sh
@@ -5,5 +5,7 @@ rm src/main/scala/spinallab/soc/DrawCtrl.scala
 rm src/main/scala/spinallab/soc/ApbGpio.scala
 mv src/main/scala/spinallab/soc/ApbGpioStudent.scala src/main/scala/spinallab/soc/ApbGpio.scala
 rm -rf .git
+rm -rf tmp
+rm -rf simWorkspace
 rm student.sh
 echo "Do not forget to clean the toplevel"
